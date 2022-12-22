eb14c58c83bd8535f84c484c45f4ab9458dd5d8e
Adam Trager <hello@adamtrager.com>
2022-12-22 16:31:37 -0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,18 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'chartjs-adapter-spacetime',
-      // the proper extensions will be added
       fileName: 'chartjs-adapter-spacetime',
     },
     rollupOptions: {
-      // // make sure to externalize deps that shouldn't be bundled
-      // // into your library
       external: ['chart.js', 'spacetime'],
-      // output: {
-      //   // Provide global variables to use in the UMD build
-      //   // for externalized deps
-      globals: {
-        'chart.js': 'Chart',
-        spacetime: 'spacetime',
+      output: {
+        globals: {
+          'chart.js': 'Chart',
+          spacetime: 'spacetime',
+        },
       },
-      // },
     },
   },
 })
